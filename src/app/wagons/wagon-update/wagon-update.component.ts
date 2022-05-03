2d1662a7cbb1c63c5b1ab7b6be646679756e64e0
bagolyr <roland.bagoly@gmail.com>
2022-05-03 16:44:20 +0200

    2 files modified

--- src/app/wagons/wagon-update/wagon-update.component.ts
+++ src/app/wagons/wagon-update/wagon-update.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WagonTable } from '../../data/wagons';
 import { WagonService } from '../../wagon.service';
@@ -21,9 +22,26 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./wagon-update.component.css'],
 })
 export class WagonUpdateComponent implements OnInit {
-  constructor(private router: Router) {}
+  wagonForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  //constructor(private router: Router) {}
 
   ngOnInit() {
-    console.log(this.router.url);
+    this.wagonForm = this.formBuilder.group({
+      identifier: '',
+      date_of_production: '',
+      track_number: '',
+      owner: '',
+      siteID: '',
+      is_deleted: false,
+    });
   }
+
+  onSubmit(data) {}
 }
